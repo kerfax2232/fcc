110bc5653b870c5adbdbd92807ed07efbcf1d9ff
kerfax2232 <kerfax22@gmail.com>
2017-07-13 11:14:14 -0700

    cleaned up, added search for array js doc

---
+++ javascript/searchStringsForWords.js
@@ -0,0 +1,25 @@
+/* 
+/ = beginning of regular expression
+Followed by text to search
+/ = end of regular expression
+g = search globally
+i = search for all instances
+*/
+
+// Setup
+var testString = "Ada Lovelace and Charles Babbage designed the first computer and the software that would have run on it.";
+
+// Example
+var expressionToGetSoftware = /software/gi;
+var softwareCount = testString.match(expressionToGetSoftware).length;
+  
+
+// Only change code below this line.
+
+var expression = /and/gi;  // Change this Line
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var andCount = testString.match(expression).length;
+
